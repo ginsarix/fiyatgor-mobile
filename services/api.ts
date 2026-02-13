55d5108d434b1d0d3de01acf6761b0f7f87bf561
filizddemir <filiz.demir@std.hku.edu.tr>
2026-02-13 09:56:26 +0300

    Removed tabs

--- services/api.ts
+++ services/api.ts
@@ -1,40 +1,40 @@
 export type Product = {
-  name: string;
-  price: string;
-  currency: string;
+    name: string;
+    price: string;
+    currency: string;
 };
 
 export type ProductResponse = {
-  product?: Product;
-  message: string;
+    product?: Product;
+    message: string;
 };
 
-const API_URL = "http://192.168.1.104:3000";
+const API_URL = "http://192.168.1.166:3000";
 
 export async function getProductByBarcode(serverCode: string, barcode: string) {
-  try {
-    const res = await fetch(
-      `${API_URL}/servers/${serverCode}/products/${barcode}`,
-    );
-
-    const text = await res.text(); // önce text al
-
-    console.log(text);
-
-    if (!res.ok) {
-      throw new Error(`Server Error: ${text}`);
-    }
-
-    // JSON parse etmeyi biz kontrol edelim
     try {
-      const json = JSON.parse(text);
+        const res = await fetch(
+            `${API_URL}/servers/${serverCode}/products/${barcode}`,
+        );
 
-      return json as ProductResponse;
-    } catch {
-      throw new Error("Server JSON dönmüyor");
+        const text = await res.text(); // önce text al
+
+        console.log(text);
+
+        if (!res.ok) {
+            throw new Error(`Server Error: ${text}`);
+        }
+
+        // JSON parse etmeyi biz kontrol edelim
+        try {
+            const json = JSON.parse(text);
+
+            return json as ProductResponse;
+        } catch {
+            throw new Error("Server JSON dönmüyor");
+        }
+    } catch (error) {
+        console.error("Fetch error:", error);
+        return undefined;
     }
-  } catch (error) {
-    console.error("Fetch error:", error);
-    return undefined;
-  }
 }
